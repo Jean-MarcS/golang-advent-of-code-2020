0c631fed178b0f4c130cc85130895e4d543a7d82
Jean-Marc <jean-marc.salis@g-softs.com>
2020-12-06 17:58:48 +0100

    Day 5

--- day 5/day5.go
+++ day 5/day5.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -13,6 +14,16 @@ func part1(line string) int {
 	row, _ := strconv.ParseInt(strings.Replace(strings.Replace(line[0:7], "F", "0", -1), "B", "1", -1), 2, 64)
 	col, _ := strconv.ParseInt(strings.Replace(strings.Replace(line[7:], "R", "1", -1), "L", "0", -1), 2, 64)
 	return int((row * 8) + col)
+}
+
+func part2(seats []int) int {
+	sort.Ints(seats)
+	for i := 1; i < len(seats); i++ {
+		if seats[i-1]+1 != seats[i] {
+			return seats[i-1] + 1
+		}
+	}
+	return -1
 }
 
 func main() {
@@ -27,6 +38,7 @@ func main() {
 
 	var maxSeatID int
 	maxSeatID = 0
+	var seats []int
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -34,8 +46,10 @@ func main() {
 		if currentSeatID > maxSeatID {
 			maxSeatID = currentSeatID
 		}
+		seats = append(seats, currentSeatID)
 	}
 
 	fmt.Println("Part 1 : ", maxSeatID)
+	fmt.Println("Part 2 : ", part2(seats))
 
 }

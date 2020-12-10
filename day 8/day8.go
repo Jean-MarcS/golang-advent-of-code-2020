fa5bfb792397f10f73b529af7040877c9d6af61e
Jean-Marc <jean-marc.salis@g-softs.com>
2020-12-10 16:39:19 +0100

    Day 8

--- day 8/day8.go
+++ day 8/day8.go
@@ -15,10 +15,10 @@ type instrType struct {
 	used bool
 }
 
-func part1(instructions []instrType) int {
+func part2(instructions []instrType, totalSize int) int {
 	pt := 0
 	accumulator := 0
-	for !instructions[pt].used {
+	for !instructions[pt].used && pt < totalSize {
 		instructions[pt].used = true
 		switch instructions[pt].code {
 		case "nop":
@@ -31,7 +31,12 @@ func part1(instructions []instrType) int {
 
 		}
 	}
-	return accumulator
+	if pt == totalSize {
+		//fmt.Println(pt, accumulator)
+		return accumulator
+	} else {
+		return 0
+	}
 }
 
 func main() {
@@ -55,7 +60,49 @@ func main() {
 		cpt++
 	}
 
-	fmt.Println("Part 1 : ", part1(instructions))
-	fmt.Println("Part 2 : ")
+	// Part 1
+	pt := 0
+	accumulator := 0
+	for !instructions[pt].used {
+		instructions[pt].used = true
+		switch instructions[pt].code {
+		case "nop":
+			pt++
+		case "jmp":
+			pt += instructions[pt].val
+		case "acc":
+			accumulator += instructions[pt].val
+			pt++
+
+		}
+	}
+
+	fmt.Println("Part 1 : ", accumulator)
+
+	accumulator = 0
+	// I know all of the instruction that can be changed (in "used") so let's only bruteforce those
+	for pt := range instructions {
+		if instructions[pt].used {
+			tmpInstructions := make([]instrType, totalSize+1)
+			copy(tmpInstructions, instructions) // Make a copy
+			for tmpPt := range tmpInstructions {
+				// reinit visited
+				tmpInstructions[tmpPt].used = false
+			}
+			if tmpInstructions[pt].code == "nop" {
+				tmpInstructions[pt].code = "jmp"
+			} else {
+				if tmpInstructions[pt].code == "jmp" {
+					tmpInstructions[pt].code = "nop"
+				}
+			}
+			test := part2(tmpInstructions, totalSize)
+			if test != 0 {
+				accumulator = test
+			}
+		}
+	}
+
+	fmt.Println("Part 2 : ", accumulator)
 
 }

3942d190bc5979e0d2e0b551e95d1829f80be39e
Jean-Marc <jean-marc.salis@g-softs.com>
2020-12-19 22:06:48 +0100

    Day 12 - part 2

--- day 12/day12.go
+++ day 12/day12.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
@@ -51,6 +52,62 @@ func part1(lines [size]instruction) int {
 	return x + y
 }
 
+func part2(lines [size]instruction) int {
+
+	x := 0
+	y := 0
+
+	wpx := 10
+	wpy := -1
+
+	for cpt := 0; cpt < size; cpt++ {
+		switch lines[cpt].action {
+		case 'N':
+			wpy -= lines[cpt].value
+		case 'S':
+			wpy += lines[cpt].value
+		case 'E':
+			wpx += lines[cpt].value
+		case 'W':
+			wpx -= lines[cpt].value
+		case 'L': // Inverted
+			switch lines[cpt].value {
+			case 90:
+				dummy := -wpx
+				wpx = wpy
+				wpy = dummy
+			case 180:
+				wpx = -wpx
+				wpy = -wpy
+			case 270:
+				dummy := -wpy
+				wpy = wpx
+				wpx = dummy
+			}
+		case 'R':
+			switch lines[cpt].value {
+			case 270:
+				dummy := -wpx
+				wpx = wpy
+				wpy = dummy
+			case 180:
+				wpx = -wpx
+				wpy = -wpy
+			case 90:
+				dummy := -wpy
+				wpy = wpx
+				wpx = dummy
+			}
+		case 'F':
+			x += wpx * lines[cpt].value
+			y += wpy * lines[cpt].value
+		}
+
+	}
+
+	return int(math.Abs(float64(x + y)))
+}
+
 func main() {
 	file, err := os.Open("day12.data")
 	if err != nil {
@@ -70,6 +127,6 @@ func main() {
 	}
 
 	fmt.Println("Part 1 :", part1(lines))
-	fmt.Println("Part 2 :")
+	fmt.Println("Part 2 :", part2(lines))
 
 }
